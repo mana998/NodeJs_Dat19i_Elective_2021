a3884c9eb5e5ef0dd0feefcfb7911087db65e8dd
Marianna <mana998@azet.sk>
2021-02-27 21:32:33 +0100

    Added version route

--- 2._Date_Time_Server/app.js
+++ 2._Date_Time_Server/app.js
@@ -16,6 +16,7 @@ app.get("/time", (req, res) => {
 
 app.get("/day", (req, res) => {
     now = new Date();
+    //new Date().toLocaleDateString('dk-DK', {weekday: 'long'})
     let date = now.getDate();
     let day = now.getDay().toString();
     res.send({day : `${date}. ${days[day]}`});
@@ -25,6 +26,11 @@ app.get("/month", (req, res) => {
     now = new Date();
     let month = now.getMonth();
     res.send({month : months[month]});
+})
+
+//route called about wchich gives version of API in a string
+app.get("/about", (req, res) => {
+    res.send({version : "1.1"});
 })
 
 app.listen(8080);
