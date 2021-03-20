c14b40e63360b7c15e50d7f17c0cb3933912b8f0
Marianna <mana998@azet.sk>
2021-03-20 08:26:37 +0100

    Added jquery for menu and footer

---
+++ 6._Mandatory_Assignment/public/js/scripts.js
@@ -0,0 +1,13 @@
+const menu = `<div class = "menu">
+<ul class = "menu menuList">
+    <li class="navButton"><a href="/">Getting Started</a></li>
+    <li class="navButton"><a href="/json">JSON</a></li>
+    <li class="navButton"><a href="/http">Rest API</a></li>
+    <li class="navButton"><a href="/javascript">Javascript</a></li>
+</ul>
+</div>`;
+
+const footer = `<footer>&copy; 4th Semester Node.js Mandatory Project Marianna Zimmermannova 2021</footer>`;
+
+$("body").prepend(menu);
+$("body").append(footer);
