05248e48e7f776c72efd7104bd925e4d8e1d4386
Marianna <mana998@azet.sk>
2021-04-01 13:54:02 +0200

    Added access to json file

--- 3._Data_server/app.js
+++ 3._Data_server/app.js
@@ -3,6 +3,13 @@ const app = express();
 
 app.use(express.json());
 
+//get cat.json
+const cat = require("./cat.json");
+
+app.get("/cat", (req, res) => {
+    res.send(cat);
+})
+
 app.get("/querystring", (req, res) => {
     res.send({"key" : req.query.key});
 })
@@ -20,4 +27,10 @@ app.post("/user", (req, res) => {
     })
 })
 
-app.listen(8080);
+//second argument takes a callback that takes an error as an argument
+app.listen(8080, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 8080);
+});
