af12456309ac8d64c56675cc813ea887f659e703
Marianna <mana998@azet.sk>
2021-04-01 13:52:31 +0200

    Added map and for in example

--- 0._Misc/loops.js
+++ 0._Misc/loops.js
@@ -1,15 +1,26 @@
 // map, reduce, filter, find
 
 const fruits = ["apples", "banana", "blueberries"];
-
+for (fruit in fruits) console.log(fruit);
 //map to print
 fruits.map((fruit) => console.log(fruit));
-
+const newFruit = fruits.map(fruit => fruit); //returns fruit
 const coppiedFruits = fruits.map((fruit, index) => {
     return { id: index, fruit, delicious: "yes yes yes"};
 });
 
 console.log(coppiedFruits);
+
+const members = [
+    {
+        username: "Lia",
+        candies: 5
+    }, 
+    {
+        username: "Leon",
+        candies: 3
+    }, 
+]
 
 people[
     {
