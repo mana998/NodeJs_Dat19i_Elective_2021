6088b406b6f1bf5cf62ad3afedbeb24f5f601cef
Marianna <mana998@azet.sk>
2021-04-15 13:54:29 +0200

    Added dynamic rendering for projects

--- 7._Nodefolio/public/projects/projects.js
+++ 7._Nodefolio/public/projects/projects.js
@@ -1,27 +1,27 @@
-class Project {
-    constructor (name, linkGit, linkDeploy, description, img, tags) {
-        this.name = name;
-        this.linkGit = linkGit;
-        this.linkDeploy = linkDeploy;
-        this.description = description;
-        this.img = img;
-        this.tags = tags;
-    }
-
-    generateProject(){
-        return(
-        `<article class="grid-container">
-        <img class="grid-item picture project-img" src="/projects/img/${this.img}.png">
-        <div class="grid-item description">
-            <h2 class="project-name">${this.name}</h2>
-            <a href="${this.linkGit}" target="_blank" class="icon-link"><img class="mini-icon" src="/projects/img/placeholder.png"></a>
-            <a href="${this.linkDeploy}" target="_blank" class="icon-link"><img class="mini-icon" src="/projects/img/placeholder.png"></a>
-            <p>${this.description}</p>
-        </div>
-    </article>`);
-    }
+function generateProject(project){
+    return(
+    `<article class="grid-container">
+    <img class="grid-item picture project-img" src="/projects/img/${project.img}.png">
+    <div class="grid-item description">
+        <h2 class="project-name">${project.title}</h2>
+        <a href="${project.gitURL}" target="_blank" class="icon-link"><img class="mini-icon" src="/projects/img/placeholder.png"></a>
+        <a href="${project.deployURL}" target="_blank" class="icon-link"><img class="mini-icon" src="/projects/img/placeholder.png"></a>
+        <p>${project.description}</p>
+    </div>
+</article>`);
 }
 
-//export function for creating skill components to follow SSR
-//else it would be CSR after JS loads in the browser
-module.exports = Project;
+(async function renderProjects() {
+    let filter = window.location.search;
+    const filterRegex = /\?filter=(.*[^&=])/;
+    filter = (filter.replace(filterRegex, "$1"));
+    const fetchString = filter ? `/api/projects?filter=${filter}` : "/api/projects";
+    const response = await fetch(fetchString);
+    const result = await response.json();
+    if (result.projects) {
+        result.projects.map(project => {
+            $("main").append(generateProject(project));
+        });
+    }
+})();
+
