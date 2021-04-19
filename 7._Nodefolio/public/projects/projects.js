db58ad10fb86435326b2218832ab8f88b711e713
Marianna <mana998@azet.sk>
2021-04-19 23:27:39 +0200

    Added icons and 'no projects found' option

--- 7._Nodefolio/public/projects/projects.js
+++ 7._Nodefolio/public/projects/projects.js
@@ -1,11 +1,11 @@
 function generateProject(project){
     return(
     `<article class="grid-container">
-    <img class="grid-item picture project-img" src="/projects/img/${project.img}.png">
+    <a href="/projects/img/${project.img}.png" target="none"><img class="grid-item picture project-img" src="/projects/img/${project.img}.png"></a>
     <div class="grid-item description">
         <h2 class="project-name">${project.title}</h2>
-        <a href="${project.gitURL}" target="_blank" class="icon-link"><img class="mini-icon" src="/projects/img/placeholder.png"></a>
-        <a href="${project.deployURL}" target="_blank" class="icon-link"><img class="mini-icon" src="/projects/img/placeholder.png"></a>
+        <a href="${project.gitURL}" target="_blank" class="icon-link"><img class="mini-icon" src="/skills/img/github.svg"></a>
+        <a href="${project.deployURL}" target="_blank" class="icon-link"><img class="mini-icon" src="/projects/img/play.svg"></a>
         <p>${project.description}</p>
     </div>
 </article>`);
@@ -21,10 +21,12 @@ async function renderProjects(max, append) {
     if (max) fetchString += filter ? `&max=${max}` : `?max=${max}`;
     const response = await fetch(fetchString);
     const result = await response.json();
-    if (result.projects) {
+    if (result.projects.length) {
         result.projects.map(project => {
             $(append).append(generateProject(project));
         });
+    } else {
+        $(append).append("<h2>No projects found</h2>");
     }
 };
 
